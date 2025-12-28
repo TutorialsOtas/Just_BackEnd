524b656334d94be60d5ab865f4ef85a639f5e0e9
Okai Tettey-Antie Samuel <teesam615@gmail.com>
2025-12-28 12:28:18 +0000

    resolving post-issues

--- js-Backend/src/app.js
+++ js-Backend/src/app.js
@@ -2,4 +2,15 @@ import express from 'express';
 
 const app = express(); //create express app
 
+app.use(express.json()); //middleware to parse json
+
+//iporting routes
+import userRouter from './routes/user.routes.js';
+
+//declaration of routes
+app.use("/api/v1/users" , userRouter);
+
+//example route : http://localhost:5000/api/v1/users/register
+
+
 export default app;
